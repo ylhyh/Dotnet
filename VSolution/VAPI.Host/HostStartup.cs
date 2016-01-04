3448ed1860db8857af12d578ae8b683f4c9da435
ylhuyh <ylhuyh@163.com>
2016-01-05 01:45:02 +0800

    Build a virtual structure and init Owin Http Hosting.

---
+++ VSolution/VAPI.Host/HostStartup.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VSolution.VAPI.Host
+{
+    class HostStartup
+    {
+        public void Configuration(IAppBuilder appBuilder)
+        {
+            //appBuilder.Run(HandleRequest);
+        }
+        static Task HandleRequest(IOwinContext context)
+        {
+            context.Response.ContentType = "text/plain";
+            return context.Response.WriteAsync("Hello, world!");
+        }
+    }
+}
---
+++ VSolution/VAPI.Host/Program.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Owin.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VAPI.Host
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Instruction:
+             * 1. Add NuGet package: Microsoft.Owin.Hosting
+             * 2. Add NuGet package: Microsoft.Owin.Host.HttpListener 
+             * The Microsoft.Owin.Host.HttpListener assembly is a runtime reference in WebApp.Start. You need to include it in the project's references for it to be available for loading.
+             */
+            using (WebApp.Start(url: "http://localhost:8800"))
+            {
+                Console.WriteLine("API host is running...");
+                Console.ReadLine();
+            }
+        }
+    }
+}
